c6e8e0a337da022904811261fa9a83b7dc9f5240
michael mason <michael@michaeljmason.com>
2017-05-01 12:38:17 -0700

    changes to staff singles

--- contact.php
+++ contact.php
@@ -11,16 +11,30 @@
       <div class="row">
         <div class="col-md-12 text-center">
           <img src="/wp-content/uploads/2017/04/TRUE-NORTH-LOGO-COLOR-2016.png" class="img-fluid">
-          <p style="margin-top:1em;">Have any questions about our services? Contact us directly by phone, email or the form provided. A representative will be with you as soon as possible.</p>
+          <h5 style="margin-top:1em;line-height: 3rem;font-weight: 300;">Have any questions about our services? Contact us directly by phone, email or the form provided. A representative will be with you as soon as possible.</h5>
         </div>
+        <hr style="border-top: 1px solid rgba(0,0,0,.1);height: 1px;width: 100%;">
         <div class="col-md-12 text-center">
           <p>2183 Fairview Rd, Suite 100</p>
           <p>Costa Mesa, CA 92627</p>
-          <a href="https://www.google.com/maps/place/2183+Fairview+Rd+%23100,+Costa+Mesa,+CA+92627/@33.6537412,-117.9104342,17z/data=!3m1!4b1!4m5!3m4!1s0x80dcdf9c3cf375ed:0x893a4fb449770b66!8m2!3d33.6537412!4d-117.9082455" target="_blank"><i class="fa fa-map"></i> Get Directions</a>
         </div>
-        <div class="col-md-12 text-center">
-          <h6><a href="#"><i class="fa fa-mobile"></i> 888.888.8888</a></h6>
-          <h6><a href="#"><i class="fa fa-envelope"></i> info@truenorthlabs.com</a></h6>
+        <div class="col-md-4 text-center">
+          <a href="#">
+          <i class="fa fa-mobile fa-5x"></i>
+          <p>Phone</p>
+          </a>
+        </div>
+        <div class="col-md-4 text-center">
+          <a href="https://www.google.com/maps/place/2183+Fairview+Rd+%23100,+Costa+Mesa,+CA+92627/@33.6537412,-117.9104342,17z/data=!3m1!4b1!4m5!3m4!1s0x80dcdf9c3cf375ed:0x893a4fb449770b66!8m2!3d33.6537412!4d-117.9082455" target="_blank">
+          <i class="fa fa-map fa-5x"></i>
+          <p>Directions</p>
+          </a>
+        </div>
+        <div class="col-md-4 text-center">
+          <a href="#">
+          <i class="fa fa-envelope fa-5x"></i>
+          <p>Email</p>
+        </a>
         </div>
       </div>
     </div>
--- templates/content-single.php
+++ templates/content-single.php
@@ -1,20 +1,19 @@
 <?php while (have_posts()) : the_post(); ?>
+<?php $title = get_field('staff_title'); ?>
   <div class="container">
     <div class="row single-post-wrap">
   <main class="col-md-12 head">
     <article <?php post_class(); ?>>
       <div class="row">
-      <div class="col-md-9">
-          <img src="<?php the_post_thumbnail_url(); ?>" class="img-fluid">
+      <div class="col-md-4">
+          <img src="<?php the_post_thumbnail_url(); ?>" class="img-fluid rounded">
       </div>
-    </div>
-      <h1 class="single-post-title"><?php the_title(); ?></h1>
-          <div class="blog-meta">
-            <?php get_template_part('templates/entry-meta'); ?>
-          </div>
-      <div class="entry-content">
+      <div class="entry-content col-md-8">
+        <h1 class="single-post-title"><?php the_title(); ?></h1>
+        <h4><?php echo $title; ?></h4>
         <?php the_content(); ?>
       </div>
+    </div>
         <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
       <!-- <?php comments_template('/templates/comments.php'); ?> -->
     </article>
